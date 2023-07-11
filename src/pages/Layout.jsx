f62fe8e2e8b5eeb7bbac0b48acdb3a3861d54672
ranapp <aryaaranadive@gmail.com>
2023-07-11 00:13:11 -0700

    layout changes

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Navbar from "/Users/aryaranadive/Projects/personal_website/arya_website/src/NavBar.jsx";
+import Navbar from "/Users/aryaranadive/Projects/ranapp.github.io/src/NavBar.jsx";
 
 const Layout = () => {
     return (
