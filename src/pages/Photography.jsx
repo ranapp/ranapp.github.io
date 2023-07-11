a962bad8ceea1fb3ef8a506404e17217769e238d
ranapp <aryaaranadive@gmail.com>
2023-07-10 17:13:16 -0700

    photography page edits

--- src/pages/Photography.jsx
+++ src/pages/Photography.jsx
@@ -3,32 +3,29 @@ import "photoswipe/dist/photoswipe.css";
 import { Gallery, Item } from "react-photoswipe-gallery";
 import "../styles/photography.css";
 
-// Import all images from the images folder
-const images = require.context('../images', false, /\.(jpg)$/);
-const imageKeys = images.keys();
-
 function Photography() {
-    console.log(imageKeys);
-   
-    return (
-        <div className='gallery'>
-            <Gallery>
-                {imageKeys.map((imageKey) => (
-                    <Item
-                        key={imageKey}
-                        original={images(imageKey).default}
-                        thumbnail={images(imageKey).default}
-                        width="1024"
-                        height="768"
-                    >
-                        {({ ref, open }) => (
-                            <img className="gallery-thumbnail" ref={ref} onClick={open} src={images(imageKey).default} alt=''/>
-                        )}
-                    </Item>
-                ))}
-            </Gallery>
-        </div>
-    );
+  const imageKeys = ["a.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg","11.jpg","12.jpg","13.jpg","14.jpg","15.jpg","16.jpg","17.jpg",
+  "18.jpg","19.jpg","20.jpg","21.jpg","22.jpg","23.jpg","24.jpg","25.jpg","26.jpg","27.jpg","28.jpg","29.jpg","30.jpg","31.jpg","32.jpg","33.jpg","34.jpg","35.jpg","36.jpg","37.jpg","38.jpg","39.jpg"]; // Add your image file names here
+
+  return (
+    <div className='gallery'>
+      <Gallery>
+        {imageKeys.map((imageKey) => (
+          <Item
+            key={imageKey}
+            original={`/images/${imageKey}`} // Update the path to match your image location within the public folder
+            thumbnail={`/images/${imageKey}`} // Update the path to match your image location within the public folder
+            width="600"
+            height="500"
+          >
+            {({ ref, open }) => (
+              <img className="gallery-thumbnail" ref={ref} onClick={open} src={`/images/${imageKey}`} alt="" />
+            )}
+          </Item>
+        ))}
+      </Gallery>
+    </div>
+  );
 }
 
 export default Photography;
