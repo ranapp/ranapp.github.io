3318fd3bd3d7586e191f9f4490863e5aa2aa15d4
ranapp <aryaaranadive@gmail.com>
2023-07-12 13:52:48 -0700

    styling for webfront projects page done

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -16,9 +16,11 @@ function Projects() {
             <div className="projects-container">
                 <div className="projectcard">
                     <h2 className="project-title">Predicting Student Academic Success</h2>
-                    <img className="project-img" src={collegesuccess} alt=""></img>
-                    <img className="project-img" src={collegesuccess2} alt=""></img>
-                    <p className="tools">Tools/Technologies Used: Python (Matplotlib, Numpy, Scikit-learn, Flask), JupyterNotebooks, Git</p>
+                    <div className='img-card1'>
+                    <img className="project-img" id="img1" src={collegesuccess} alt=""></img>
+                    <img className="project-img"  id="img2" src={collegesuccess2} alt=""></img>
+                    </div>
+                    <p className="tools"> <span class="bold-text"><span class="bold-text">Tools/Technologies Used: </span> </span> Python (Matplotlib, Numpy, Scikit-learn, Flask), JupyterNotebooks, Git</p>
                     <div className="description">
                         <p>College education is an important part of many people’s lives. To many people it is one of the biggest opportunities that can jumpstart someone’s career and livelihood. However, everyone that goes to college comes from different backgrounds and live in different ways while at college. This can have an enormous impact on the success of the student with respect to their college career and their graduation. It could be beneficial to analyze these different factors and somehow relate them to the anticipated success of the students. </p>
                         <p>This project was worked on with a group of four people. We used the dataset from Kaggle, ‘Predicting Student Dropout and Academic Success’, which contained 4424 student records and 35 features before data preprocessing and one-hot encoding. We built six different models (Polynomial Regression with Sigmoid, Logistic Regression, Neural Network, SVM, Decision Tree, and Naive Bayes) and were able to construct a model with 91% accuracy using Logistic Regression with K-Fold Cross Validation.
@@ -41,9 +43,11 @@ function Projects() {
         }}/>
                 <div className="projectcard">
                     <h2 className="project-title">ConGRADS Degree Check Website</h2>
-                    <img className="project-img" src={congrads1}alt=""></img>
-                    <img className="project-img" src={congrads2}alt=""></img>
-                    <p className="tools">Tools/Technologies Used: Python, React, Node.js, Express.js, MongoDB, Google OAuth 2.0, HTML/CSS, Git</p>
+                    <div className='img-card2'>
+                    <img className="project-img" id="img3" src={congrads1}alt=""></img>
+                    <img className="project-img" id="img4" src={congrads2}alt=""></img>
+                    </div>
+                    <p className="tools"> <span class="bold-text"><span class="bold-text">Tools/Technologies Used: </span> </span>Python, React, Node.js, Express.js, MongoDB, Google OAuth 2.0, HTML/CSS, Git</p>
                     <div className="description">
                         <p>ConGrads is a degree check web application. The user can input their college, major, minor, and upload a copy of their transcript which will then display a UI with a progress bar letting the user know how much of the graduation requirement percentage they have met. ConGrads also lets users know which classes they need to take after cross checking their classes on transcript with our database class requirements. My team and I received this source information from the UC Davis degree requirements webpage. Our goal was to implement this web application with Human-Computer Interaction concepts. Some principles we followed were that we took the heuristic evaluation ruleset (recognition rather than recall) into consideration because our solution allows the user to not have to recall which choices they made (school, major, minor) by providing a review page which shows the options users picked, and be able to edit and make changes on that same review page instead of restarting the entire process. We also tap into the emotional aspect of the user by providing/welcoming with customized messages rather than having standard robotic messages/prompts.
                         </p>
@@ -71,8 +75,8 @@ function Projects() {
                 <div className="projectcard">
                     <h2 className="project-title">Effect of Informational Value on Attentional Guidance </h2>
                     <p className="subtitle">UC Davis Center for Mind and Brain - Geng Lab</p>
-                    <img className="project-img" src={genglab}alt=""></img>
-                    <p className="tools">Tools/Technologies Used: Python (Numpy, Scipy, Seaborn, Scikit-learn), JupyterNotebooks</p>
+                    <img className="project-img" id="img5" src={genglab}alt=""></img>
+                    <p className="tools"><span class="bold-text"><span class="bold-text">Tools/Technologies Used: </span> </span>Python (Numpy, Scipy, Seaborn, Scikit-learn), JupyterNotebooks</p>
                     <div className="description">
                         <p>Our environment is complex and dynamic, making it critical for observers to determine what information in the environment is important for achieving their goals. In this study that I worked on under the mentorship of Philip Witkowski Dr. Joy Geng, we tested whether attention is moderated by the perceived reliability of information used to predict upcoming stimuli. Trials were coded with Python and published on SONA.
                         </p>
@@ -94,8 +98,8 @@ function Projects() {
         }}/>
                 <div className="projectcard">
                     <h2 className="project-title">Water Levels Web App</h2>
-                    <img className="project-img" src={waterlevels1}alt=""></img>
-                    <p className="tools">Tools/Technologies Used: Javascript, React, Node.js, Express</p>
+                    <img className="project-img" id="img6" src={waterlevels1}alt=""></img>
+                    <p className="tools"><span class="bold-text"><span class="bold-text">Tools/Technologies Used: </span> </span> Javascript, React, Node.js, Express</p>
                     <div className="description">
                         <p>The Water Levels Web Application is a simple application that displays information about the water levels in California’s seven largest reservoirs. This project was completed successfully with a group of three people. The frontend was coded using React and displays a simple description and a button that displays a chart using Chart.js. The frontend sends an AJAX POST request to the backend to get the data given a month and the backend handles an API call to get reservoir data from the California Data Exchange Center, which is a government provided data service. Each time the month and the year change by selecting on the calendar display, a new AJAX request for data is sent. </p>
                         <p className="Links">
@@ -115,8 +119,8 @@ function Projects() {
         }}/>
                 <div className="projectcard">
                     <h2 className="project-title">Web Scraping Indeed.com</h2>
-                    <img className="project-img" src={scraping}alt=""></img>
-                    <p className="tools">Tools/Technologies Used: R, Git</p>
+                    <img className="project-img" id="img7" src={scraping}alt=""></img>
+                    <p className="tools"><span class="bold-text">Tools/Technologies Used: </span> R, Git</p>
                     <div className="description">
                         <p>With hundreds of job postings each day, there is so much variety and data associated with each posting. I wanted to analyze this data and compile it into a database that users can use when needing help looking for jobs. </p>
                         <p>This project was coded in R, using the XML library and Regex for web scraping. The website was scraped by first looking at the job search (Data Scientist) and then going through each individual posting in that category and getting the data needed (Company, Date Posted, Salary, Work Type, etc). These components were compiled into a single dataframe for each job search. </p>
@@ -141,8 +145,8 @@ function Projects() {
                 <div className="projectcard">
                     <h2 className="project-title">Artruism Web App</h2>
                     <p className='subtitle'>#include</p>
-                    <img className="project-img" src={artruism}alt=""></img>
-                    <p className="tools">Tools/Technologies Used: Javascript, Next.js, CSS</p>
+                    <img className="project-img" id="img8" src={artruism}alt=""></img>
+                    <p className="tools"><span class="bold-text">Tools/Technologies Used: </span> Javascript, Next.js, CSS</p>
                     <div className="description">
                         <p>I worked on a project with #include, a local Davis-run organization that builds web application for businesses in the Davis/Sacramento area, for a local non-profit called Artruism. Artruism is an organization that creates a platform for student artists to help serve their community in different ways. I worked with a team of people as a frontend developer to construct a page of the web application. </p>
                         <p className="Links">
@@ -162,7 +166,7 @@ function Projects() {
         }}/>
                 <div className="projectcard">
                     <h2 className="project-title">Network Flow Tutors</h2>
-                    <p className="tools">Tools/Technologies Used: C++</p>
+                    <p className="tools"><span class="bold-text">Tools/Technologies Used: </span> C++</p>
                     <div className="description">
                         <p>The goal of this project was to find a way to assign tutors willing to tutor for the CS department to the courses that they preferred to teach. The tutors filled out the preferences of the classes that they wanted to tutor and the algorithm uses that data to assign each tutor to a class. </p>
                         <p>The algorithm was constructed using the Ford-Fulkerson Algorithm and Bipartite matching. Each tutor and course have a corresponding node. </p>
