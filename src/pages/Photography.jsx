5a6332a50374ea1e2395e481f84af36b2fdeb38d
ranapp <aryaaranadive@gmail.com>
2023-07-12 19:30:19 -0700

    images save

--- src/pages/Photography.jsx
+++ src/pages/Photography.jsx
@@ -4,8 +4,8 @@ import { Gallery, Item } from "react-photoswipe-gallery";
 import "../styles/photography.css";
 
 function Photography() {
-  const imageKeys=['img_39.jpg', 'img_38.jpg', 'img_37.jpg', 'img_36.jpg', 'img_35.jpg', 'img_34.jpg', 'img_33.jpg', 'img_32.jpg', 'img_30.jpg', 'img_31.jpg', 'img_28.jpg',  'img_25.jpg', 'img_26.jpg','img_24.jpg','img_22.jpg', 'img_23.jpg', 'img_6.jpg', 'img_21.jpg', 'img_20.jpg', 'img_19.jpg', 'img_18.jpg', 'img_17.jpg', 'z.jpg', 'img_15.jpg', 'img_4.jpg', 'img_13.jpg', 'img_14.jpg', 'img_10.jpg', 'img_11.jpg', 'img_12.jpg','img_16.jpg', 'img_8.jpg', 'img_7.jpg',  'img_5.jpg',  'img_3.jpg', 'img_2.jpg', 'a.jpg'];
-  const width = ['600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px'];
+  const imageKeys=['img_39.jpg', 'img_38.jpg', 'img_37.jpg', 'img_36.jpg', 'img_35.jpg', 'img_34.jpg', 'img_33.jpg', 'img_32.jpg', 'img_30.jpg', 'img_31.jpg', 'img_28.jpg',  'img_25.jpg', 'img_26.jpg','img_24.jpg','img_22.jpg', 'img_23.jpg', 'img_6.jpg', 'img_21.jpg', 'img_20.jpg', 'img_19.jpg', 'img_18.jpg', 'img_17.jpg', 'img_9.jpg', 'img_15.jpg', 'img_4.jpg', 'img_13.jpg', 'img_14.jpg', 'img_10.jpg', 'img_11.jpg', 'img_12.jpg','img_16.jpg', 'img_8.jpg', 'img_7.jpg',  'img_5.jpg',  'img_3.jpg', 'img_2.jpg', 'a.jpg'];
+  const width = ['600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px', '600px'];
   const height =['500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px', '500px'];
   console.log(imageKeys);
   return (
@@ -16,13 +16,13 @@ function Photography() {
             className = "img-gallery"
             key={imageKey}
             id={imageKey}
-            original={`public/images/${imageKey}`} 
-            thumbnail={`public/images/${imageKey}`} 
+            original={require(`../images/${imageKey}`).default} 
+            thumbnail={require(`../images/${imageKey}`).default} 
             width={width[index]}
             height={height[index]}
           >
             {({ ref, open }) => (
-              <img className="gallery-thumbnail" ref={ref} onClick={open} src={`/images/${imageKey}`} alt="" />
+              <img className="gallery-thumbnail" ref={ref} onClick={open} src={require(`../images/${imageKey}`).default} alt="" />
             )}
           </Item>
         ))}
